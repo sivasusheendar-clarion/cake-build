d748c0e4dfced88848bc9818cd491214615c9f89
sivasusheendar-clarion <sivasusheendar.t@clariontechnologies.co.in>
2013-08-21 19:08:36 +0530

    db schema token added

--- app/Config/Schema/cake_build.sql
+++ app/Config/Schema/cake_build.sql
@@ -31,10 +31,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `role` varchar(20) DEFAULT NULL,
+  `token` varchar(150) NOT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `users`
